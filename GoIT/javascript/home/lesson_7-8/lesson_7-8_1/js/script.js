9c72973381b42576e7719262f72be1c9f611e469
Yulia Prokopenko <yul.prokopenko@gmail.com>
2015-12-29 18:35:47 +0200

    tabs without CSS

---
+++ GoIT/javascript/home/lesson_7-8/lesson_7-8_1/js/script.js
@@ -0,0 +1,36 @@
+// jQuery | $
+
+alert('BOOOOOO');
+
+	var $wrapperTabs = $('<div class="wrapper"></div>');
+	
+	var $contents = $('<div class="tab_content"></div>');
+	var $items = $('<div class="article1"></div><div class="article2"></div><div class="article3"></div>');
+	$items.addClass("tab_item");
+
+
+
+	var $tabs = $('<div class="tabs"></div>');
+	var $tab = $('<span>Nunc tincidunt</span><span>Proin dolor</span><span>Aenean lacinia</span>');
+	$tab.addClass("tab");
+
+
+	$('body').append($wrapperTabs);
+	$wrapperTabs.append($tabs);
+	$wrapperTabs.append($contents);
+	$contents.append($items);
+	$tabs.append($tab);
+
+
+	$('.article1').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quos, alias nam aliquam quibusdam placeat nesciunt repellendus nostrum earum consectetur et laboriosam, numquam voluptatum corrupti quis, quod architecto repudiandae ullam.');
+	$('.article2').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem optio hic eveniet tempore quae amet. Vel est hic ea, adipisci? Ducimus consequatur cum iste laboriosam recusandae, eius a atque veritatis.');
+	$('.article3').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur similique quam distinctio placeat quisquam tempora magni ad. Voluptatum, facilis, magnam. Et rerum porro, voluptate facilis ad tempore corrupti explicabo magni.');
+
+
+	$(".tab_item").not(":first").hide();
+	$(".wrapper .tab").click(function() {
+	$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+
