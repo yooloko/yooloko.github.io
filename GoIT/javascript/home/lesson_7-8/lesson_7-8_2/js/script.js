7adda251eb5d2ed1579b42cc0789c2a496cb9e73
Yulia Prokopenko <yul.prokopenko@gmail.com>
2016-01-09 11:54:33 +0200

    without CSS, button rough draft added

--- GoIT/javascript/home/lesson_7-8/lesson_7-8_2/js/script.js
+++ GoIT/javascript/home/lesson_7-8/lesson_7-8_2/js/script.js
@@ -1,17 +1,17 @@
 // jQuery | $
 
- // alert('BOOOOOO');
+ alert('BOOO!1');
 
  (function() {
  	var $toolTip = $('<div class="tooltip"></div>');
- 	debugger;
+ 	// debugger;
  	$('input[id]').each(function(){
  		var $this = $(this),
  		title = $this.attr('title');
  		// this.title = "";
- 		debugger;
+ 		// debugger;
  		$this.mouseover(function(e){
- 			debugger;
+ 			// debugger;
  			$toolTip
  				.append(title)
  				.prependTo('form')
@@ -28,11 +28,26 @@
 
  })();
 
-	// $('.show-help').on('click', function(){
-	// 	$('.tooltip').animate({opacity: 1}, 300);
-	// 	setTimeout(function(){
-	// 		$(".tooltip").stop().animate({ "opacity": 0 },300);
-	// 	}, 2000);
-	// })
+ $('.show-help').on('click', function(){
+ 	alert('BOOO!2');
+ 	var titleArr = [];
+ 	$('input').each(function(){
+ 		titleArr.push($(this).attr('title'));
+ 		console.log('title', $(this).attr('title'));
+ 		debugger;
+ 	});
 
-  	
+ 	// ??var toolTipArr = [<div class='tool-tip-arr'><div/>, <div class='tool-tip-arr'><div/>, <div class='tool-tip-arr'><div/>];
+ 	var i = 0;
+ 	$('.tool-tip-arr').each(function(){
+ 	$(this)
+ 			.append('titleArr[i]')
+ 			.prependTo('form')
+ 			.hide()
+ 		.css({
+ 			top: e.pageY + 10,
+ 			left: e.pageX + 30
+ 		}).fadeIn(300);
+ 		i++;
+ 	});
+ });
